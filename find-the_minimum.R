3aacbec0211f8e11ca1e7b226fb1f784669ae447
Mayo <kadanganorris@gmail.com>
2024-01-23 05:35:01 +0100

    a function that finds the minimum value

---
+++ find-the_minimum.R
@@ -0,0 +1,30 @@
+#!/usr/bin/R
+
+# A function that find a minimun value within a numeric vector
+find_min<- function(x) {
+     if (!is.numeric(x)) {
+          stop("Input must be a numeric vector.")
+     }
+
+     if (length(x) == 0) {
+          warning("Input vector is empty.")
+          return(NA)
+     }
+
+     min_val <- x[1]
+
+     for (val in x) {
+          if (val < min_val) {
+               min_val <- val
+          }
+     }
+
+     return(min_val)
+}
+#Test the function
+set.seed(33)
+x<-runif(50)
+find_min(x)
+min(x)
+z_letter<-sample(letters, 6)
+find_min(z_letter)
